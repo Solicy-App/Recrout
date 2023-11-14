98d6997e6f460ae9568eff33759a857523aab785
Vigen <vigen.hovhannisyan@solicy.net>
2023-11-14 10:08:49 +0400

    added reset password service

--- services/auth/auth.ts
+++ services/auth/auth.ts
@@ -25,4 +25,27 @@ export class AuthService {
       console.log(error);
     }
   }
+  public static async resetPassword(creedentials: FormData): Promise<void> {
+    try {
+      const res = await this.apiBase.postAsync(
+        `/en/auth/password/reset/?r=${Math.random()}`,
+        creedentials,
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public static async passwordDone(creedentials: FormData): Promise<void> {
+    try {
+      const res = await this.apiBase.postAsync(
+        `/en/auth/password/reset/done/?r=${Math.random()}`,
+        creedentials,
+      );
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
